d5fe8551407541779d3c81882c8d9d527b055ac4
Net2Dev <info@a3b.io>
2022-05-29 21:29:03 -0700

    Add files via upload

---
+++ Part4-ABI-Config-Import/config.js
@@ -0,0 +1,5 @@
+export const NFTCONTRACT = "0xC6Ad2824B03275D4cC5E8f4f61c5a143b999717b";
+export const STAKINGCONTRACT = "0x09aE75315fE2E63271B4F218C4b00F4fF143052A";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/";
