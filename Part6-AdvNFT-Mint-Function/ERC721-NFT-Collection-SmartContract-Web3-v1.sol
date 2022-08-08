36b96db697c0abccbb5706a516cbfbfeacbcab75
Net2Dev <info@a3b.io>
2022-08-07 20:32:22 -0700

    Update ERC721-NFT-Collection-SmartContract-Web3-v1.sol

--- Part6-AdvNFT-Mint-Function/ERC721-NFT-Collection-SmartContract-Web3-v1.sol
+++ Part6-AdvNFT-Mint-Function/ERC721-NFT-Collection-SmartContract-Web3-v1.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT LICENSE
 
-*/
+/*
 Follow/Subscribe Youtube, Github, IM, Tiktok
 for more amazing content!!
 @Net2Dev
@@ -16,7 +16,7 @@ FOR ITS USE. I AM NOT RESPONSIBLE FOR ANY
 OTHER USE. THIS IS TRAINING/EDUCATIONAL
 MATERIAL. ONLY USE IT IF YOU AGREE TO THE
 TERMS SPECIFIED ABOVE.
-/*
+*/
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
